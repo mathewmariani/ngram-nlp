cf58f61f2512f26a3fececefa39f9a216fd02de3
Mat Mariani <mat.mariani@hotmail.com>
2019-04-28 10:23:36 -0400

    Added German language

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,12 @@
 # Changelog
 
 
+### 0.0.2
+
+- Increased corpus size to 30k lines of text per language.
+- Added German as a detectable language.
+
+
 ### 0.0.1
 
 - Initial release.
--- src/assets/js/components/inputsentence.js
+++ src/assets/js/components/inputsentence.js
@@ -2,13 +2,15 @@ Vue.component('input-sentence', {
 	props: {
 		unigram_en: Array,
 		unigram_fr: Array,
+		unigram_ge: Array,
 	},
 	data: function() {
 		return {
 			validchars: "abcdefghijklmnopqrstuvwxyz",
 			sentence: "",
 			p_en: 1.0,
-			p_fr: 1.0
+			p_fr: 1.0,
+			p_ge: 1.0
 		}
 	},
 	methods: {
@@ -16,6 +18,7 @@ Vue.component('input-sentence', {
 			this.sentence = ""
 			p_en = 1.0
 			p_fr = 1.0
+			p_ge = 1.0
 		},
 		sanitize: function (str) {
 			return str.toLowerCase().replace(/[^a-z]/ig, '')
@@ -24,7 +27,7 @@ Vue.component('input-sentence', {
 			{	// english
 				this.p_en = 1.0
 				const d = 0.05
-				const s = d * (1.0 / 5789.0)
+				const s = d * (1.0 / 938871.0)
 				
 				const sentence = this.sanitize(this.sentence)
 				for (const char of sentence) {
@@ -34,11 +37,21 @@ Vue.component('input-sentence', {
 			{	// french
 				this.p_fr = 1.0
 				const d = 0.05
-				const s = d * (1.0 / 5268.0)
+				const s = d * (1.0 / 2834055.0)
 				
 				const sentence = this.sanitize(this.sentence)
 				for (const char of sentence) {
 					this.p_fr += Math.log(((1.0 - d) * this.unigram_fr[char] + s))
+				}
+			}
+			{	// german
+				this.p_ge = 1.0
+				const d = 0.05
+				const s = d * (1.0 / 2759238.0)
+				
+				const sentence = this.sanitize(this.sentence)
+				for (const char of sentence) {
+					this.p_ge += Math.log(((1.0 - d) * this.unigram_ge[char] + s))
 				}
 			}
 		},
@@ -65,10 +78,20 @@ Vue.component('input-sentence', {
 				<li class="list-group-item">
 					Log Probability French: {{ p_fr }}
 				</li>
+				<li class="list-group-item">
+					Log Probability German: {{ p_ge }}
+				</li>
 			</ul>
-			<div class="card-body" v-show="(p_en !== p_fr)">
-				<p class="lead">This sentence is {{ (p_en > p_fr) ? "English" : "French" }}</p>
+			<div class="card-body" v-show="((p_en > p_fr) && (p_en > p_ge))">
+				<p class="lead">This sentence is English</p>
 			</div>
+			<div class="card-body" v-show="((p_fr > p_en) && (p_fr > p_ge))">
+				<p class="lead">This sentence is French</p>
+			</div>
+			<div class="card-body" v-show="((p_ge > p_fr) && (p_ge > p_en))">
+				<p class="lead">This sentence is German</p>
+			</div>
+
 		</div>
 	`
 });
