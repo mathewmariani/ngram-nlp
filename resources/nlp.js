294ec0a7813011ccd07b453d9cee2e0e8ca3fca9
Mathew Mariani <mat.mariani@hotmail.com>
2025-01-22 11:42:45 -0500

    Made nlp.js an es module (#4)

--- resources/nlp.js
+++ resources/nlp.js
@@ -1,25 +1,27 @@
 const nlp = {
     sanitize: (str) => {
-        return str.toLowerCase().replace(/[^a-z ]/ig, '');
+        return str.toLowerCase().replace(/[^a-z ]/ig, '')
     },
 
     laplaceSmoothing: (d, f, s) => {
-        return Math.log((1.0 - d) * f + s);
+        return Math.log((1.0 - d) * f + s)
     },
 
     probability: (text, count, frequencies) => {
-        let p = 0.0;
-        const d = 0.05;
-        const s = d * (1.0 / count);
-        const n = 2;
+        let p = 0.0
+        const d = 0.05
+        const s = d * (1.0 / count)
+        const n = 2
 
-        text = nlp.sanitize(text);
+        text = nlp.sanitize(text)
         for (let i = 0; i <= text.length - n; i++) {
-            const ngram = text.slice(i, i + n);
-            const f = frequencies[ngram] || 0;
-            p += nlp.laplaceSmoothing(d, f, s);
+            const ngram = text.slice(i, i + n)
+            const f = frequencies[ngram] || 0
+            p += nlp.laplaceSmoothing(d, f, s)
         }
 
-        return p;
+        return p
     },
-};
+}
+
+module.exports = nlp
